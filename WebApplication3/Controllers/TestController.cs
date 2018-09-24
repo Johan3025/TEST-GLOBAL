d6e64205b1dc9a3f77dfcfe4d2c1fcec406005bf
johan <arjohan1822@hotmail.com>
2018-09-24 00:04:49 -0500

    se termino desarrollo

---
+++ WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Data;
+using Newtonsoft.Json.Linq;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class TestController : Controller
+    {
+        public Employees Employee = new Employees(); 
+
+        // GET: Test
+        public ActionResult Index()
+        {
+            String baseUrl = "http://masglobaltestapi.azurewebsites.net/api/Employees";
+            var json = new WebClient().DownloadString(baseUrl);
+
+        
+
+            JArray PersonArray = JArray.Parse(json);
+
+            IList<Employees> persona = PersonArray.Select(p => new Employees
+            {
+                name = (string)p["name"],
+                contractTypeName = (string)p["contractTypeName"],
+                roleId = (string)p["roleId"],
+                roleName = (string)p["roleName"],
+                roleDescription = (string)p["roleDescription"],
+                hourlySalary = (int)p["hourlySalary"],
+                monthlySalary = (int)p["monthlySalary"]
+                
+            }).ToList();
+
+
+            foreach (var item in persona)
+            {
+                if (item.contractTypeName == "HourlySalaryEmployee")
+                {
+                    item.Total = 120 * item.hourlySalary * 12;
+                }
+                else
+                {
+                    item.Total = item.monthlySalary * 12;
+                }
+
+            }
+           
+            return View(persona);
+        }
+    }
+}
